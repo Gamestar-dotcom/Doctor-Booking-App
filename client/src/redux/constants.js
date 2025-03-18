1d5fd383d301841a1993ce9ab34b8f414d4185db
dotcom <gamestaraficionado@gmail.com>
2025-03-18 05:40:02 +0300

    update from local host to use render urls

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,6 +1,6 @@
-export const users_url = "http://localhost:5000/api/users";
-export const doctors_url = "http://localhost:5000/api/doctor";
-export const BASE_URL = "http://localhost:5000/api";
-export const appointment_url = "http://localhost:5000/api/appointment";
-export const payments_url = "http://localhost:5000/api/payment";
-export const admin_url = "http://localhost:5000/api/admin";
+export const BASE_URL = "https://doctor-booking-backend-qw5l.onrender.com/api";
+export const users_url = `${BASE_URL}/users`;
+export const doctors_url = `${BASE_URL}/doctor`;
+export const appointment_url = `${BASE_URL}/appointment`;
+export const payments_url = `${BASE_URL}/payment`;
+export const admin_url = `${BASE_URL}/admin`;
